0b524569c3a38b0cdc65d35fdd282c62131f80e7
EngNelson <bechem200@gmail.com>
2023-11-28 15:44:29 +0100

    solution

---
+++ src/api/features/logistics/expeditions/cancel-expedition/cancel-expedition.controller.ts
@@ -0,0 +1,84 @@
+import {
+    Body,
+    Controller,
+    NotFoundException,
+    Post,
+    Query,
+    Req,
+    UseGuards,
+  } from '@nestjs/common';
+  import { ApiResponse, ApiTags } from '@nestjs/swagger';
+  import {
+    Abilities,
+    AgentRoles,
+    ApiRessources,
+    ISOLandDto,
+    ISOLang,
+    RequirePrivileges,
+    Ressource,
+    Roles,
+    UserCon,
+    UserConnected,
+  } from '@glosuite/shared';
+  import { JwtAuthGuard, PermissionsGuard, RolesGuard } from 'src/api/guards';
+//   import { AddDeliveryInput } from './dto';
+//   import { AddDeliveryService } from './add-delivery.service';
+import { AddExpeditionInput } from './dto';
+import {AddExpeditionService} from './add-expedition.service'; 
+  
+  
+  
+  @ApiTags('expedition')
+  @Controller('expedition')
+  export class AddExpeditionController {
+    constructor(private readonly _addExpeditionService: AddExpeditionService) {}
+  
+    @UseGuards(JwtAuthGuard, RolesGuard, PermissionsGuard)
+    @Roles(
+      AgentRoles.SUPER_ADMIN,
+      AgentRoles.ADMIN,
+      AgentRoles.PUS_COORDINATOR,
+      AgentRoles.PUS_MANAGER,
+      AgentRoles.PUS_AGENT,
+      AgentRoles.PUS_CASHIER,
+      AgentRoles.SAV_AGENT,
+      AgentRoles.SAV_MANAGER,
+      AgentRoles.STOCK_AGENT,
+      AgentRoles.PICK_PACK,
+      AgentRoles.FLEET_SUPERVISOR,
+      AgentRoles.WAREHOUSE_MANAGER,
+      AgentRoles.LOGISTIC_MANAGER,
+      AgentRoles.CUSTOMER_SERVICE,
+      AgentRoles.CUSTOMER_SERVICE_SUPERVISOR,
+    )
+    @Ressource(ApiRessources.EXPEDITION)
+    @RequirePrivileges(Abilities.MANAGE, Abilities.CREATE)
+    @Post()
+    @ApiResponse({
+      status: 201,
+      type: AddExpeditionInput,
+    })
+  async cancelExpedition(
+    @Body() body: AddExpeditionInput,
+    @Query() params: ISOLandDto,
+    @UserConnected() user: UserCon,
+    @Req() request: any,
+  ): Promise<AddExpeditionInput>{
+    if(!user){
+    throw new NotFoundException(`User not Found`);
+  }
+  
+  body.lang = params.lang
+  ? params.lang
+  : user.preferedLang
+  ? user.preferedLang
+  : ISOLang.FR;
+  
+  const accessToken = request.headers.authorization.replace('Bearer', '');
+  return await this._addExpeditionService.addExpedition(body, user, accessToken);
+  }
+  
+  }
+  
+  
+  
---
+++ src/api/features/logistics/expeditions/cancel-expedition/cancel-expedition.service.ts
@@ -0,0 +1,182 @@
+import {
+    BadRequestException,
+    HttpException,
+    HttpStatus,
+    Injectable,
+  } from '@nestjs/common';
+  import { InjectRepository } from '@nestjs/typeorm';
+  import {
+    AgentRoles,
+    FROM_EMAIL,
+    isNullOrWhiteSpace,
+    ISOLang,
+    UserCon,
+  } from '@glosuite/shared';
+  import { Delivery } from 'src/domain/entities/logistics';
+  import { DeliveryRepository } from 'src/repositories/logistics';
+  import { DeliveryStatus } from 'src/domain/enums/logistics';
+  import { AddDeliveryInput } from './dto';
+  import { OrderRepository } from 'src/repositories/orders';
+  import { Order } from 'src/domain/entities/orders';
+  import { AddInternalNeedService } from 'src/api/features/flows/internal-need/add-internal-need/add-internal-need.service';
+  import { USERS_RESOURCE } from 'src/domain/constants';
+  import { EmailInputModel } from 'src/domain/interfaces';
+  import { SendingEmailService } from 'src/services/email';
+  import { HttpService } from '@nestjs/axios';
+  
+  
+  type ValidationResult = {
+    delivery: AddDeliveryInput;
+    lang: ISOLang;
+    user: UserCon;
+  };
+  
+  @Injectable()
+  export class AddExpeditionService {
+    constructor(
+      @InjectRepository(Delivery)
+      private readonly _deliveryRepository: DeliveryRepository,
+      @InjectRepository(Order)
+      private readonly _orderRepository: OrderRepository,
+      private readonly _sendingEmailService: SendingEmailService,
+      private readonly _httpService: HttpService,
+    ) { }
+  
+    async addExpedition(
+      input: AddExpeditionInput,
+      user: UserCon,
+      accessToken: string,
+    ): Promise<AddExpeditionInput> {
+  
+      const validationResult = await this._tryValidation(input, user);
+  
+      if (!validationResult) {
+        throw new HttpException('Input validation error', HttpStatus.BAD_REQUEST);
+      }
+  
+      const executionResult = await this._tryExecution(
+        accessToken,
+        validationResult,
+      );
+      if (!executionResult) {
+        throw new HttpException(
+          'Error Ocured During Execution',
+          HttpStatus.CREATED,
+        );
+      }
+      return executionResult;
+    }
+  
+    private async _tryExecution(
+      accessToken: string,
+      result: ValidationResult,
+    ): Promise<AddDeliveryInput> {
+  
+  
+      try {
+        // Set Delivery Status
+        result.expedition.status = ExpeditionStatus.PENDING;
+  
+        // Send Notification To Delivery Agent
+        const sendEmailTo: string[] = [];
+        const path = `${process.env.AUTH_API_PATH}/${USERS_RESOURCE}`;
+        await this._httpService.axiosRef
+          .get(path + `?roles=${AgentRoles.WAREHOUSE_MANAGER}`, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              'Accept-Encoding': 'gzip,deflate,compress',
+            },
+          })
+          .then((response) => {
+            console.log(
+              `${response.config.method} on ${response.config.url}. Result=${response.statusText}`,
+              'Data ',
+              response.data,
+            );
+  
+            response.data.items.map((item) => {
+              if (item) {
+                sendEmailTo.push(item.email);
+              }
+            });
+          })
+          .catch((error) => {
+            throw new HttpException(
+              error.message,
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+          });
+  
+        sendEmailTo.unshift(result.user.email);
+  
+        if (sendEmailTo.length > 0) {
+          console.log(`Send mail to ${sendEmailTo}`);
+  
+          const emailInput: EmailInputModel = {
+            to: sendEmailTo,
+            from: FROM_EMAIL,
+            subject: 'Material Requisition Form',
+          };
+  
+          try {
+            await this._sendingEmailService.sendEmail(emailInput);
+          } catch (error) {
+            console.log(
+              `Error sending email: ${error} - ${AddInternalNeedService.name} - ${this._tryExecution.name}`,
+            );
+          }
+        }
+  
+        // Save Delivery Data To DB And Send Response To Frontend
+        return await this._deliveryRepository.save(result.delivery);
+  
+      } catch (error) {
+        throw new HttpException(error?.message, HttpStatus.BAD_REQUEST);
+      }
+    }
+  
+    private async _tryValidation(
+      input: AddExpeditionInput,
+      user: UserCon,
+    ): Promise<ValidationResult> {
+      try {
+        const lang = input.lang
+          ? input.lang
+          : user.preferedLang
+            ? user.preferedLang
+            : ISOLang.FR;
+  
+        // Check For Null or White Space Values
+        if (
+          isNullOrWhiteSpace(input.orderId)
+        ) {
+          throw new BadRequestException(
+            `OrderId is required`,
+          );
+        }
+  
+        if (
+          isNullOrWhiteSpace(input.transportation)
+        ) {
+          throw new BadRequestException(
+            `transportationMeans is required`,
+          );
+        }
+  
+        // Check If OrderId Exists
+        const orderData = await this._orderRepository.findOne(input.orderId);
+        if (!orderData) {
+          throw new HttpException(`Order with id ${input.orderId} not found`, HttpStatus.NOT_FOUND);
+        }
+  
+        return { delivery: input, user, lang };
+      } catch (error) {
+        console.log('Here is todays message ', error.message);
+  
+        throw new BadRequestException("Validation failed");
+      }
+  
+    }
+  
+  }
+  
